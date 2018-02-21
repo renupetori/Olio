5741691f7a66e37c307ca5fd1ff1860a9f2b70ef
Petri Laine <31991591+renupetori@users.noreply.github.com>
2018-02-21 19:52:50 +0200

    Auto tehtävä
    Tuosta

---
+++ Auto_luokka/Auto_luokka/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_luokka
+{
+    class Car
+    {
+        public string Brand;
+        public int Speed;
+
+        public Car(int AccelerateVal)
+        {
+            AskData();
+            ShowCarInfo();
+            Accelerate(AccelerateVal);
+            Brake();
+        }
+
+        public void AskData()
+        {
+            Console.WriteLine("Syötä auton merkki: ");
+            Brand = Console.ReadLine();
+            Console.WriteLine("Syötä sen nopeus: ");
+            Speed = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki: {Brand}\nAuton nopeus: {Speed}km/h");
+        }
+
+        public void Accelerate()
+        {
+            
+        }
+
+        public void Accelerate(int addedSpeed)
+        {
+            if (addedSpeed > 0)
+            {
+                Speed = Speed + addedSpeed;
+                Console.WriteLine($"Auton nopeus kiihdytyksen jälkeen on: {Speed}km/h");
+            }
+            else
+            {
+                Console.WriteLine($"Negatiivinen muutos ei ole sallittu kiihdytyksessä.");
+            }
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine($"Auton nopeus jarrutuksen jälkeen on: {Speed * 0.9}km/h\n");
+        }
+    }
+}
---
+++ Auto_luokka/Auto_luokka/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Auto_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar1 = new Car(30);
+            Console.ReadKey();
+
+            Car newCar2 = new Car(-10);
+            Console.ReadKey();
+        }
+    }
+}
