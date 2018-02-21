4d656415f6c2819665cf632cc2a3f7bac8a6a51d
Petri Laine <31991591+renupetori@users.noreply.github.com>
2018-02-21 19:20:45 +0200

    Kirja tehtävä
    Pieni kiire hups

---
+++ Kirja luokka/Kirja luokka/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirja_luokka
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+        
+        public Book()
+        {
+            Title = "Ohrana";
+            Author = "Jari Tervo";
+            Id = 01234;
+            Price = 8.90;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+        
+        public void PrintBook()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title}\nKirjailija: {Author}\nID: {Id}\nHinta: {Price} euroa\n");
+            Console.ReadKey();
+        }
+
+        public string Compare(Book book)
+        {
+            if (this.Price > book.Price)
+            {
+                return ($"{this.Title} on kalliimpi kuin {book.Title}.");
+            }
+            else
+            {
+                return ($"{book.Title} on kalliimpi kuin {this.Title}.");
+            }
+        }
+    }
+}
---
+++ Kirja luokka/Kirja luokka/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kirja_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book("Pahan polku", "Robert Galbraith", 00254, 8.50);
+
+            book1.PrintBook();
+            book2.PrintBook();
+
+            Console.WriteLine(book1.Compare(book2));
+            Console.ReadKey();
+        }
+    }
+}
