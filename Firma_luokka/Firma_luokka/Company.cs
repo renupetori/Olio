0bc81e8b47d869dea188c9fe9018c1d9eab474c7
Petri Laine <31991591+renupetori@users.noreply.github.com>
2018-02-21 20:43:26 +0200

    Firma luokka
    hetken ihmettelin kopiointimuodostimen tarkoitusta mutta ihan kätsy

---
+++ Firma_luokka/Firma_luokka/Company.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firma_luokka
+{
+    class Company
+    {
+        public string Title;
+        public string Address;
+        public string Phone;
+        public double Outcome;
+        public double Expense;
+
+        public Company()
+        {
+
+        }
+
+        public Company(string title, string address, string phone, double outcome, double expense)
+        {
+            Title = title;
+            Address = address;
+            Phone = phone;
+            Outcome = outcome;
+            Expense = expense;
+            ShowCompany();
+            Profit();
+        }
+
+        public Company(Company previousCompany)
+        {
+            Title = previousCompany.Title;
+            Address = previousCompany.Address;
+            Phone = previousCompany.Phone;
+            Outcome = previousCompany.Outcome;
+            Expense = previousCompany.Expense;
+            ShowCompany();
+            Profit();
+        }
+
+        public void ShowCompany()
+        {
+            Console.WriteLine($"Yritys: {Title}\nOsoite: {Address}\nPuhelin: {Phone}\nTulot: {Outcome} euroa\nMenot: {Expense} euroa");
+        }
+
+        public void Profit()
+        {
+            double profit = Math.Round((Outcome - Expense) / Expense * 100);
+
+            if (profit >= 300)
+            {
+                Console.WriteLine($"Yrityksen voitto on {profit}% suurempi kuin menot. Firmalla menee hyvin.\n");
+            }
+            else if (profit >= 200)
+            {
+                Console.WriteLine($"Firman voitto on {profit}% suurempi kuin menot. Firmalla menee tyydyttävästi.\n");
+            }
+            else if (profit < 100)
+            {
+                Console.WriteLine($"Firman voitto on {profit}% suurempi kuin menot. Firmalla menee kehnosti.\n");
+            }
+        }
+    }
+}
---
+++ Firma_luokka/Firma_luokka/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Firma_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company1 = new Company("Kaluste Oy","Suokatu 3","045 1234 567",12000.00,9500.00);
+            Console.ReadKey();
+
+            Company company2 = new Company(company1.Title,company1.Address,company1.Phone,20000.00,3000.00);
+            Console.ReadKey();
+
+            Company company3 = new Company(company1);
+            Console.ReadKey();
+        }
+    }
+}
