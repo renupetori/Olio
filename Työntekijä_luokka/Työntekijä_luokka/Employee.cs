69c44179abfa3942d993cb1669bd3360a004ff9b
Petri Laine <31991591+renupetori@users.noreply.github.com>
2018-02-21 21:20:46 +0200

    Työntekijä luokka
    mukava pieni tehtävä

---
+++ Työntekijä_luokka/Työntekijä_luokka/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Työntekijä_luokka
+{
+    class Employee
+    {
+        string Name;
+        int Id;
+        string Position;
+        double Salary;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+            PrintEmployeeInfo();
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijä: {Name}\nId: {Id}\nTyötehtävä: {Position}\nPalkka: {Salary} euroa\n");
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+            if (Salary > employee.Salary)
+            {
+                return ($"Työntekijän {Name} palkka on suurempi kuin työntekijän {employee.Name} palkka.\n");
+            }
+            else if (Salary < employee.Salary)
+            {
+                return ($"Työntekijän {Name} palkka on pienempi kuin työntekijän {employee.Name} palkka.\n");
+            }
+            else
+            {
+                return ($"Työntekijän {Name} palkka on yhtäsuuri kuin työntekijän {employee.Name} palkka.\n");
+            }
+        }
+    }
+}
---
+++ Työntekijä_luokka/Työntekijä_luokka/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Työntekijä_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[4];
+            employees[0] = new Employee("Rudi Rautiainen", 1, "Toimitusjohtaja", 8000.00);
+            employees[1] = new Employee("Teemu Menttä", 2, "Myyjä", 2500.00);
+            employees[2] = new Employee("Nils Hansson", 3, "Siistijä", 2500.00);
+            employees[3] = new Employee("Johan Mattsson", 4, "Maskotti", 2000.00);
+
+            Console.WriteLine(employees[1].CompareSalary(employees[0]));
+            Console.WriteLine(employees[1].CompareSalary(employees[2]));
+            Console.WriteLine(employees[1].CompareSalary(employees[3]));
+
+            Console.ReadKey();
+        }
+    }
+}
